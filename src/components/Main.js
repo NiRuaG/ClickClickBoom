8abeb97559352e1111982275c349a7555ab860dd
NiRuaG <gaurin1107@gmail.com>
2019-01-31 17:49:29 -0600

    extend array to all possible 'mons

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,55 +6,72 @@ import nameMap from './../names';
 
 import shuffle from './../util/shuffle';
 
+import './main.css'
+
 class Main extends Component {
   state = {
+    maxScore: nameMap.length-1, //! '-1' because 0-index name mapping
+
+    locked: false,
+
     curScore: 0,
-    maxScore: 12,
     bestScore: 0,
-    numbers: [...Array(13).keys()].splice(1),
-    clicked: [],
+    numbers: [...Array(nameMap.length).keys()].splice(1), //! '..splice(1)' because 0-index name mapping
     clickedSet: new Set()
   }
 
 
   handleClick(keyNum) {
-    let { curScore } = this.state;
 
-
-    const numbers = [...this.state.numbers];
-    shuffle(numbers);
-
-    const clicked = [...this.state.clicked];
-
-    const clickedSet = new Set(this.state.clickedSet);
-    if (clickedSet.has(keyNum)) {
-      clicked.push(`DUPLICATE\n${nameMap[keyNum]}`);
+    if (this.state.clickedSet.has(keyNum)) {
+      this.setState({
+        clickedSet: new Set(),
+        curScore: 0
+      });
     }
+
     else {
-      ++curScore;
-      clickedSet.add(keyNum);
-      clicked.push(nameMap[keyNum]);
-    }
 
-    this.setState({
-      curScore,
-      numbers,
-      clicked,
-      clickedSet
-    });
+      // Add this new keyNum to Set
+      const clickedSet = new Set(this.state.clickedSet);
+      clickedSet.add(keyNum);
+
+      // Shuffle the tiles
+      const numbers = [...this.state.numbers];
+      shuffle(numbers);
+
+      // Score updates
+      let { curScore, bestScore } = this.state;
+      if (++curScore > bestScore) {
+        bestScore = curScore;
+      }
+
+
+      this.setState({
+        curScore,
+        bestScore,
+        numbers,
+        clickedSet
+      });
+
+    }
+    
   }
 
   render() {
 
-    const clickcards = this.state.numbers.map(n => {
-      const name = nameMap[n];
+    const clickcards = this.state.numbers.map( (num, i) => {
+      const name = nameMap[num];
+      //% We have all the click cards "render to DOM" and just 'toggle' the display property in its styling as a prop
+      //% seems to help with not reloading images
       return (
         <ClickCard
-          key={n}
+          key={num}
           alt={name}
-          src={`img/${String(n).padStart(3,'0')} ${name}.png`}
-          onClick={() => this.handleClick(n)}
-          /> 
+          display={i < 12} //! only show first 12
+          src={`img/${String(num).padStart(3,'0')} ${name}.png`}
+          onClick={() => this.handleClick(num)}
+          />
       )
     });
 
@@ -63,27 +80,20 @@ class Main extends Component {
       <>
         <header className="App-header">
           <p>
-            Current Score: {`${this.state.curScore} of ${this.state.maxScore}`}
+            Current Score: <span id="curScore">{this.state.curScore}</span>{` of ${this.state.maxScore}`}
           </p>
           <p>
-            Best Score: {this.state.bestScore}
+            Best Score: <span id="bestScore">{this.state.bestScore}</span>{` of ${this.state.maxScore}`}
           </p>
         </header>
 
-        <aside style={{
-          float: 'left',
-          display: 'flex',
-          flexFlow: 'column nowrap'
-        }}>
-          {this.state.clicked.map(mon => <p>{mon}</p>)}
-        </aside>
-
         <main style={{
-          maxWidth: '50rem',
+          maxWidth: '53rem',
           margin: '0 auto',
           display: 'flex',
           flexFlow: 'row wrap',
           justifyContent: 'space-around',
+          pointerEvents: this.state.locked && 'none'
         }}>
           {clickcards}
         </main>
